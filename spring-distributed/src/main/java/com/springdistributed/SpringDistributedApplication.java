1de16280c7b593e1efa82896104728b5e8cc4f86
Mananp96 <manan96.mb@gmail.com>
2020-05-28 22:13:07 -0400

    Init Repository

---
+++ spring-distributed/src/main/java/com/springdistributed/SpringDistributedApplication.java
@@ -0,0 +1,13 @@
+package com.springdistributed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDistributedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDistributedApplication.class, args);
+	}
+
+}
---
+++ spring-distributed/src/test/java/com/springdistributed/SpringDistributedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdistributed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDistributedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
